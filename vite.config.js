325470d45e9bff9852da0ea4e03fcf9c17746e46
toby-00 <ryanmceachern93@gmail.com>
2025-05-02 17:24:01 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,7 @@
+npm create vite@latest golf-strokes-gained -- --template react
+cd golf-strokes-gained
+npm install
+npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
+npm install chart.js idb
 
