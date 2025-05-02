086d580ebdde95e4e0873e74f83828ee1a393ff0
toby-00 <ryanmceachern93@gmail.com>
2025-05-02 17:25:50 -0400

    Update strokesgainedtable.js

--- src/data/strokesgainedtable.js
+++ src/data/strokesgainedtable.js
@@ -1,1 +1,9 @@
+export const strokesGainedTable = {
+  tee: { 20: 2.4, 40: 2.6, ... },
+  fairway: { ... },
+  rough: { ... },
+  sand: { ... },
+  recovery: { ... },
+  green: { 3: 1.04, 4: 1.13, ... }
+};
 
