035452e495fc5101bbdf50e4b68a668e9503192c
toby-00 <ryanmceachern93@gmail.com>
2025-05-02 17:26:53 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,8 @@
+import { strokesGainedTable } from '../data/strokesGainedTable';
+
+export function calculateSG(startDist, endDist, lieType, isPutt = false) {
+  const from = strokesGainedTable[lieType][startDist];
+  const to = isPutt ? strokesGainedTable.green[endDist] : strokesGainedTable['green'][3];
+  return from - to - 1;
+}
 
