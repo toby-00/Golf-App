073470eeb04669eff96d8b7b0082448eb492634c
toby-00 <ryanmceachern93@gmail.com>
2025-05-02 17:25:26 -0400

    Update ds.js

--- src/lib/ds.js
+++ src/lib/ds.js
@@ -1,1 +1,24 @@
+import { openDB } from 'idb';
+
+const DB_NAME = 'golf_sg';
+const DB_VERSION = 1;
+const STORE = 'rounds';
+
+export async function initDB() {
+  return openDB(DB_NAME, DB_VERSION, {
+    upgrade(db) {
+      db.createObjectStore(STORE, { keyPath: 'id', autoIncrement: true });
+    }
+  });
+}
+
+export async function saveRound(round) {
+  const db = await initDB();
+  return db.add(STORE, round);
+}
+
+export async function getRounds() {
+  const db = await initDB();
+  return db.getAll(STORE);
+}
 
